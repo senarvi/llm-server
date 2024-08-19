6f655cfafc30be6817aaf2894f27def3a604e431
Seppo Enarvi <seppo.git@marjaniemi.com>
2024-08-19 11:47:21 +0300

    only_one_api=True has to be specified, since we don't have a base_path

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,6 @@ def client(loop, aiohttp_client):
     logging.getLogger("connexion.operation").setLevel("ERROR")
     options = {"swagger_ui": True}
     specification_dir = os.path.join(os.path.dirname(__file__), "..", "llm_server", "openapi")
-    app = connexion.AioHttpApp(__name__, specification_dir=specification_dir, options=options)
+    app = connexion.AioHttpApp(__name__, only_one_api=True, specification_dir=specification_dir, options=options)
     app.add_api("openapi.yaml", pythonic_params=True, pass_context_arg_name="request")
     return loop.run_until_complete(aiohttp_client(app.app))
