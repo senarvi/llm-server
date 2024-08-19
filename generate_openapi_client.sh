a1979f1c47f9f5e789fb33a1e930dd67e48887d3
Seppo Enarvi <seppo.git@marjaniemi.com>
2024-08-19 12:01:08 +0300

    Generate the client code

--- generate_openapi_client.sh
+++ generate_openapi_client.sh
@@ -5,9 +5,5 @@ root_dir="${script_file%/*}"
 
 openapi-generator-cli generate \
   --input-spec "${root_dir}/openapi.json" \
-  --generator-name python-aiohttp \
-  --output "${root_dir}" \
-  --package-name llm_server \
-  --additional-properties packageVersion=0.0.1
-
-make format
+  --generator-name typescript-fetch \
+  --output "${root_dir}"
---
+++ models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HTTPValidationError';
+export * from './LocationInner';
+export * from './MovieReview';
+export * from './MovieSummary';
+export * from './ValidationError';
